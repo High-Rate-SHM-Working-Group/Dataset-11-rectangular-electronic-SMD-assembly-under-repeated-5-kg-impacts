9ccf09a015e9478112e96c63699432acbd0ef2ef
hugo-luck <hugo@thelucks.org>
2025-07-22 16:28:01 -0400

    Update B46_feature_plotting.py

--- Data/Board 4.6/B46_feature_plotting.py
+++ Data/Board 4.6/B46_feature_plotting.py
@@ -10,13 +10,15 @@ import math
 import copy
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
 
-file_path = os.getcwd()
-save_path = file_path.replace("\Board 4.6", "\Board 4.6\\figures")
+current_directory = os.getcwd()
+file_path = current_directory
+save_path = current_directory.replace("\Board 4.6", "\Board 4.6\\figures")
 
 impacts = []
 feature_names = ["Maximum", "Absolute Mean", "RMS", "Skewness", "Kurtosis", "Crest Factor", "Shape Factor", "Impulse Factor"]
-features = [[] for i in range(2)]
+features = [[] for i in range(3)]
 
 lvm_files = sorted([f for f in os.listdir(file_path) if f.endswith(".lvm")])
             
@@ -27,17 +29,23 @@ for j, filename in enumerate(lvm_files):
         data_zeroed = data.copy()
         current_max = -1
         absolute_mean = 0
+        rms_value = 0
         for k in range(len(data)):
             data_zeroed[k]-=data[0]
             if abs(data_zeroed[k]) > current_max:
                 current_max = abs(data_zeroed[k])
             absolute_mean += (abs(data_zeroed[k])/len(data_zeroed))
-                
+            rms_value += pow(data_zeroed[k], 2)
+        
         features[0].append(current_max)
         features[1].append(absolute_mean)
+        features[2].append(rms_value)
         impacts.append(j)
 
 #print(features[0])
+
+for i in range(len(features[2])):
+    features[2][i] = math.sqrt(((features[2][i]/len(features[2]))))
 
 unfiltered_mean = 0
 unfiltered_standard_deviation = 0
@@ -59,17 +67,24 @@ while(not outliers_fixed):
         outliers_fixed = True
 
 #print(features[0][0])
+
 features_zeroed = copy.deepcopy(features)
 
-for i in range(len(features[0])):
-    for j in range(len(features)):
-        features_zeroed[j][i] -= features[j][0]
+#for i in range(len(features[0])):
+#    for j in range(len(features)):
+#        features_zeroed[j][i] -= features[j][0]
+        
+features_normalized = copy.deepcopy(features_zeroed)
 
-print(features[1])
+for i in range(len(features)):
+   features_normalized[i] = preprocessing.normalize([features_zeroed[i]])
+
+#print(features[1])
 
 plt.figure(figsize=(10, 6))
-plt.plot(impacts, features_zeroed[0],  marker='o', linestyle='', label=feature_names[0])
-plt.plot(impacts, features_zeroed[1],  marker='o', linestyle='', label=feature_names[1])
+plt.plot(impacts, features_normalized[0][0],  marker='o', linestyle='', label=feature_names[0])
+plt.plot(impacts, features_normalized[1][0],  marker='o', linestyle='', label=feature_names[1])
+plt.plot(impacts, features_normalized[2][0],  marker='o', linestyle='', label=feature_names[2])
 plt.legend()
 plt.xlabel('Impact Number')
 plt.ylabel('Feature Values')
