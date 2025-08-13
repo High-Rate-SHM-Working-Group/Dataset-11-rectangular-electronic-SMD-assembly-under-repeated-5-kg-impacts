7a32bd5fc139978e5e4a59073c9b9110722223f5
hugo-luck <hugo@thelucks.org>
2025-08-13 13:51:26 -0400

    Update Combined_Boards_resistance_plotting_percent_based.py

--- Data/Analysis/Combined_Boards_resistance_plotting_percent_based.py
+++ Data/Analysis/Combined_Boards_resistance_plotting_percent_based.py
@@ -19,6 +19,8 @@ data_list = os.listdir(file_path)
 impact_numbers_list = []
 resistance_numbers_list = []
 board_names = []
+board_names_trendline = []
+board_save_paths = []
 MAX_VALID_RESISTANCE = 2
 
 for i in data_list:
@@ -26,6 +28,8 @@ for i in data_list:
         if not i == "Board 0.0":
             new_file_address = file_path+"\\"+i
             board_names.append(i)
+            board_names_trendline.append(i+' Trendline')
+            board_save_paths.append(new_file_address)
             
             lvm_files = sorted([f for f in os.listdir(new_file_address) if f.endswith(".lvm")])
             
@@ -78,9 +82,9 @@ for i in range(len(board_names)):
     plt.plot(impact_numbers_percents[i], resistance_numbers_percents[i],  marker='o', linestyle='-', label=board_names[i])
 #plt.ylim([-0.0005,0.002])
 plt.legend()
-plt.xlabel('Impact Number')
-plt.ylabel('Resistance Change (Ohms)')
-plt.title('Filtered Resistance vs. Impact Number')
+plt.xlabel('Impact Percent')
+plt.ylabel('Resistance Percent')
+plt.title('Resistance Percent vs. Impact Percent')
 plt.grid(True)
 plt.tight_layout()
 plt.savefig(f"{save_path}\\all_boards_metric_plot_percent_based.png", dpi=300)
@@ -139,61 +143,36 @@ for i in range(len(impact_numbers_percents)):
     fit_line_x_values.append(np.linspace(0, 100))
     fit_line_y_values.append(y_value(a_values[i], b_values[i], fit_line_x_values[i]))
     
-plt.figure(figsize=(10,6))
+plt.figure(figsize=(12,8))
 #plt.plot(fit_line_x_values[0], fit_line_y_values[0])
-for i in range(3):
-    plt.plot(fit_line_x_values[i+4], fit_line_y_values[i+4], label=board_names[i+4])
-for i in range(3):
-    plt.plot(impact_numbers_percents[i+4], resistance_numbers_percents[i+4],  marker='o', linestyle='', label=board_names[i+4])
-plt.plot(fit_line_x_values[8], fit_line_y_values[8], label=board_names[8])
-plt.plot(impact_numbers_percents[8], resistance_numbers_percents[8],  marker='o', linestyle='', label=board_names[8])
+for i in range(len(board_names)):
+    plt.plot(fit_line_x_values[i], fit_line_y_values[i], label=board_names_trendline[i])
+for i in range(len(board_names)):
+    plt.plot(impact_numbers_percents[i], resistance_numbers_percents[i],  marker='o', linestyle='', label=board_names[i])
+#plt.plot(fit_line_x_values[8], fit_line_y_values[8], label=board_names[8])
+#plt.plot(impact_numbers_percents[8], resistance_numbers_percents[8],  marker='o', linestyle='', label=board_names[8])
+plt.ylim([-5,120])
 plt.legend()
-plt.xlabel('Impact Number')
-plt.ylabel('Resistance Change (Ohms)')
-plt.title('Filtered Resistance vs. Impact Number')
+plt.xlabel('Impact Percent')
+plt.ylabel('Resistance Percent')
+plt.title('Resistance Percent vs. Impact Percent')
 plt.grid(True)
 plt.tight_layout()
-#plt.savefig(f"{save_path}\\all_boards_metric_plot_percent_based.png", dpi=300)
+plt.savefig(f"{save_path}\\all_boards_metric_plot_percent_based_with_trendlines.png", dpi=300)
 plt.show()
 
 for i in range(len(board_names)):
     plt.figure(figsize=(10,6))
-    plt.plot(fit_line_x_values[i], fit_line_y_values[i], label=board_names[i])
+    plt.plot(fit_line_x_values[i], fit_line_y_values[i], label=board_names_trendline[i])
     plt.plot(impact_numbers_percents[i], resistance_numbers_percents[i],  marker='o', linestyle='', label=board_names[i])
+    plt.ylim([-5,120])
     plt.legend()
-    plt.xlabel('Impact Number')
-    plt.ylabel('Resistance Change (Ohms)')
-    plt.title('Filtered Resistance vs. Impact Number')
+    plt.xlabel('Impact Percent')
+    plt.ylabel('Resistance Percent')
+    plt.title('Resistance Percent vs. Impact Percent')
     plt.grid(True)
     plt.tight_layout()
-    #plt.savefig(f"{save_path}\\all_boards_metric_plot_percent_based.png", dpi=300)
+    plt.savefig(f"{board_save_paths[i]}\\{board_names[i]}_metric_plot_percent_based.png", dpi=300)
     plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
